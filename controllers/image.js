9f3e3f70f7e0047fd51857103775fbbae90110b9
Abner <sbner2@gmail.com>
2020-07-13 19:57:20 -0300

    First Commit

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai = require('clarifai');
+const app = new Clarifai.App({
+ apiKey: '7a83d2f84c08464a8507e21476482e66'
+});
+
+const handleApiCall = (req, res) => {
+	app.models.predict({
+	      id: "a403429f2ddf4b49b307e318f00e528b",
+	      version: "34ce21a40cc24b6b96ffee54aabff139",
+	    }, req.body.input)
+	.then(data => res.json(data))
+	.catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+  	db('users')
+	.where('id','=', id)
+  	.increment('entries', 1)
+ 	.returning('entries')
+ 	.then(entries =>{
+ 		res.json((entries[0]))
+ 	})
+ 	.catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage,
+	handleApiCall
+}
